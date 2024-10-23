eafd54ed47e97c6516413fef7d382a2529daccf9
navidul hoque <hnavidul@gmail.com>
2024-10-23 23:13:35 +0600

    portfolio

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -6,7 +6,7 @@ import { RxCross2 } from "react-icons/rx";
 export default function Sidebar({ listItems, isSidebarOpen, setIsSidebarOpen }) {
 
     return (
-        <ul className={`bg-[#1f0016] text-white w-[200px] h-full flex flex-col gap-y-4 fixed top-0 right-0 z-20 py-[20px] px-[20px] transition-all duration-500 ${isSidebarOpen ? "translate-x-0" : "translate-x-full"}`}>
+        <ul className={`bg-[#1f0016] text-white w-[200px] h-full flex flex-col gap-y-7 fixed top-0 right-0 z-20 py-[20px] px-[20px] transition-all duration-500 ${isSidebarOpen ? "translate-x-0" : "translate-x-full"}`}>
 
             <RxCross2 
                 className="self-end text-white w-[30px] h-[30px] cursor-pointer"
