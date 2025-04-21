a01eef819adcbf6b23fe735fe6a1599736b0a7b9
NavidulHoque <hnavidul@gmail.com>
2025-04-21 15:36:32 +0600

    Remove unused project images and update resume to reflect PostgreSQL and Prisma usage in Appointment API

--- data/projects.ts
+++ data/projects.ts
@@ -1,16 +1,12 @@
 import blogApp from "@/public/images/BlogApp.png"
 import chattingApp from "@/public/images/ChattingApp.png"
 import gemini from "@/public/images/gemini.png"
-import fileManagementSystem from "@/public/images/FileManagementSystem.png"
-import noteApp from "@/public/images/noteApp.png"
-import ToDoList from "@/public/images/TodoList.png"
 import FoodDeliveryWebsite from "@/public/images/FoodDeliveryWebsite.png"
 import FoodDeliveryWebsiteAdmin from "@/public/images/FoodDeliveryWebsite-Admin.png"
-import BgRemoval from "@/public/images/BgRemoval.png"
 import { StaticImageData } from "next/image"
 
 
-export interface Projects{
+export interface Projects {
     name: string;
     link: string;
     photo: StaticImageData;
@@ -52,43 +48,11 @@ export const projects: Projects[] = [
         description2: "Used Firebase for authentication, real-time database, along with React JS for a dynamic, responsive user interface."
     },
     {
-        name: "Background Removal Application",
-        link: "https://bgremoval23.netlify.app",
-        photo: BgRemoval,
-        description1: "Built a full stack background removal app where users can sign up with Google, GitHub, or credentials. New users get 5 free credits to remove image backgrounds. After using the credits, users need to purchase more (buying feature coming soon).",
-
-        description2: "Used NextJS, Typescript and Shadcn UI for the frontend, Next-Auth for authentication, ExpressJS for the backend, Mongoose for handling database schema and MongoDB as the database."
-    },
-    {
         name: "Gemini Clone",
         link: "https://geminiclone098.netlify.app",
         photo: gemini,
         description1: "Developed a Gemini clone that allows users to generate results almost similar to the Gemini platform",
 
         description2: "Used React JS to create a responsive interface that replicates better user experience."
-    },
-    {
-        name: "File Management System",
-        link: "https://filemanagementsystem.netlify.app",
-        photo: fileManagementSystem,
-        description1: "Developed a file management system that allows users to create private folders and files, with features to edit the data of file and delete both files and folders.",
-
-        description2: "Used React JS for a dynamic, user-friendly interface, MongoDB for secure data storage, Mongoose for handling database schema, Express JS for backend."
-    },
-    {
-        name: "Note App",
-        link: "https://noteapp098.netlify.app",
-        photo: noteApp,
-        description1: "Developed a note application where users can easily create, update, and delete notes.",
-
-        description2: "Built with React JS for a responsive user interface."
-    },
-    {
-        name: "To Do List",
-        link: "https://todolist425.netlify.app",
-        photo: ToDoList,
-        description1: "Developed a ToDoList application where users can easily create, update, and delete ToDos.",
-
-        description2: "Built with React JS for a responsive user interface."
     }
 ]
